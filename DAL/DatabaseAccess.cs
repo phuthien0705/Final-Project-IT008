b8289cbdf95f1d0eadfb814f3f2bfec3581885da
AnhTrung732 <20520332@gm.uit.edu.vn>
2022-11-24 16:44:05 +0700

    Init Three Layer Model

---
+++ DAL/DatabaseAccess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    internal class DatabaseAccess
+    {
+        // Tạo chuỗi kết nối cơ sở dữ liệu
+        public static SqlConnection Connect()
+        {
+            String strcon = @"Data Source=.;Initial Catalog=GarageManagement;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(strcon);
+            return conn; 
+        }
+    }
+}
--- DTO/Class1.cs
+++ DTO/Class1.cs
@@ -1,7 +1,12 @@
-﻿namespace DTO
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
 {
     public class Class1
     {
-
     }
 }
