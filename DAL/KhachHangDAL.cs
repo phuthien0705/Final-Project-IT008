d5c1589bd0d5b6faefac4e33d9eefe7f70e2c3e5
AnhTrung732 <93305749+AnhTrung732@users.noreply.github.com>
2022-12-14 12:51:21 +0700

    Save

--- DAL/KhachHangDAL.cs
+++ DAL/KhachHangDAL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,30 @@ namespace DAL
 {
     internal class KhachHangDAL
     {
+        private static KhachHangDAL instance;
+        public static KhachHangDAL Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new KhachHangDAL();
+                return instance;
+            }
+            private set
+            {
+                instance = value;
+            }
+        }
+        private KhachHangDAL() { }
+        public int ThemKhachHang(string ten, string sdt, string diachi)
+        {
+            string query = "ThemKhachHang @TenKH , @DienThoai , @DiaChi , @TienNo";
+            return DataProvider.Instance.ExecuteNonQuery(query, new object[] { ten, sdt, diachi, 0 });
+        }
+        public DataTable LayMaKH(string ten, string sdt)
+        {
+            string query = "SELECT MaKH FROM KHACHHANG WHERE TenKH = '" + ten + "' and DienThoai = '" + sdt + "'";
+            return DataProvider.Instance.ExecuteQuery(query);
+        }
     }
 }
--- DTO/DTO_KhachHang.cs
+++ DTO/DTO_KhachHang.cs
@@ -8,5 +8,41 @@ namespace DTO
 {
     internal class DTO_KhachHang
     {
+        public string sTenKH;
+        public string sDienThoai;
+        public string sDiaChi;
+        public int iTienNo;
+
+        public string TenKH
+        {
+            get { return sTenKH; }
+            set { sTenKH = value; }
+        }
+        public string DienThoai
+        {
+            get { return sDienThoai; }
+            set { sDienThoai = value; }
+        }
+        public string DiaChi
+        {
+            get { return sDiaChi; }
+            set { sDiaChi = value; }
+        }
+        public int TienNo
+        {
+            get { return iTienNo; }
+            set { iTienNo = value; }
+        }
+        public DTO_KhachHang(string tenkh, string dienthoai, string diachi, int tienno)
+        {
+            this.sTenKH = tenkh;
+            this.sDienThoai = dienthoai;
+            this.sDiaChi = diachi;
+            this.iTienNo = tienno;
+        }
+        public DTO_KhachHang()
+        {
+
+        }
     }
 }
