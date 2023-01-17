55799b1e3d62eed7e9ab11d3cc6c299734509d8f
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2023-01-17 22:42:58 +0700

    [fix] DataProvider in DAL

--- DAL/DataProvider.cs
+++ DAL/DataProvider.cs
@@ -3,10 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace DAL
 {
-    class DataProvider
+    public class DataProvider
     {
+        private static DataProvider instance;
+
+        private string connectionStr = "Data Source=DESKTOP-OQCKJ3E;Initial Catalog=QuanLyHocSinh;Integrated Security=True";
+
+        public static DataProvider Instance
+        {
+            get => instance == null ? new DataProvider() : instance;
+            private set => instance = value;
+        }
+
+        public DataTable ExecuteQuery(string query)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionStr))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(data);
+                connection.Close();
+            }
+            return data;
+        }
+
+        public int ExecuteNonQuery(string query)
+        {
+            int data = 0;
+            using (SqlConnection connection = new SqlConnection(connectionStr))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                data = command.ExecuteNonQuery();
+                connection.Close();
+            }
+            return data;
+        }
+
+        public object ExecuteScalar(string query)
+        {
+            object data = 0;
+            using (SqlConnection connection = new SqlConnection(connectionStr))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                data = command.ExecuteScalar();
+                connection.Close();
+            }
+            return data;
+        }
     }
 }
